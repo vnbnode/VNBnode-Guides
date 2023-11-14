2449d2f6b05c8740252a70005404e1ad3bb50041
johnt9x <40466326+johnt9x@users.noreply.github.com>
2023-11-14 10:38:01 +0700

    Update mantra.sh

--- Mantra/mantra.sh
+++ Mantra/mantra.sh
@@ -27,11 +27,10 @@ go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.5.0
 
 # Dowload binary
 cd $HOME
-sudo wget -O /usr/lib/libwasmvm.x86_64.so https://github.com/CosmWasm/wasmvm/releases/download/v1.3.0/libwasmvm.x86_64.so
-wget https://testnet-files.itrocket.net/mantra/mantrachaind-linux-amd64.zip
+wget https://github.com/MANTRA-Finance/public/raw/main/mantrachain-testnet/mantrachaind-linux-amd64.zip
 unzip mantrachaind-linux-amd64.zip
-rm mantrachaind-linux-amd64.zip
 sudo chmod +x mantrachaind
+rm mantrachaind-linux-amd64.zip
 # Setup Cosmovisor Symlinks
 mkdir -p $HOME/.mantrachain/cosmovisor/genesis/bin
 mv mantrachaind $HOME/.mantrachain/cosmovisor/genesis/bin/
