7bc0a4c497605a622002979bf8b4fecec24f8024
MFLow VNBnode <76662222+pnpn1212@users.noreply.github.com>
2023-11-17 22:27:37 +0700

    Update validator.sh

--- Avail/validator.sh
+++ Avail/validator.sh
@@ -15,24 +15,24 @@ sleep 1 && curl -s https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main
 echo -e "\e[1m\e[32m1. Download new entrypoint.sh... \e[0m" && sleep 1
 wget -q -O entrypoint.sh https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main/Avail/entrypoint.sh
 chmod +x entrypoint.sh
-CONTAINER_ID=`docker ps | egrep 'availj/avail' | awk '{print $1}'`
+NAMES=`docker ps | egrep 'availj/avail' | awk '{print $10}'`
 
 # Insert new entrypoint
-docker cp /root/avail/entrypoint.sh ${CONTAINER_ID}:/entrypoint.sh
+docker cp /root/avail/entrypoint.sh ${NAMES}:/entrypoint.sh
 rm /root/avail/validator.sh
 rm /root/avail/entrypoint.sh
 
 # Restart avail container
 echo -e "\e[1m\e[32m2. Restart avail container... \e[0m" && sleep 1
-docker restart ${CONTAINER_ID}
+docker restart ${NAMES}
 sleep 1
 
 # Command check
 echo '====================== SETUP FINISHED ======================'
-echo -e "\e[1;32mView the logs from the running: \e[0m\e[1;36msudo docker logs -f ${CONTAINER_ID}\e[0m"
+echo -e "\e[1;32mView the logs from the running: \e[0m\e[1;36msudo docker logs -f ${NAMES}\e[0m"
 echo -e "\e[1;32mCheck the list of containers: \e[0m\e[1;36msudo docker ps -a\e[0m"
-echo -e "\e[1;32mStop your avail node: \e[0m\e[1;36msudo docker stop ${CONTAINER_ID}\e[0m"
-echo -e "\e[1;32mStart your avail node: \e[0m\e[1;36msudo docker start ${CONTAINER_ID}\e[0m"
-echo -e "\e[1;32mRestart your avail node: \e[0m\e[1;36msudo docker restart ${CONTAINER_ID}\e[0m"
-echo -e "\e[1;32mRemove avail: \e[0m\e[1;36msudo docker rm ${CONTAINER_ID}\e[0m"
+echo -e "\e[1;32mStop your avail node: \e[0m\e[1;36msudo docker stop ${NAMES}\e[0m"
+echo -e "\e[1;32mStart your avail node: \e[0m\e[1;36msudo docker start ${NAMES}\e[0m"
+echo -e "\e[1;32mRestart your avail node: \e[0m\e[1;36msudo docker restart ${NAMES}\e[0m"
+echo -e "\e[1;32mRemove avail: \e[0m\e[1;36msudo docker rm ${NAMES}\e[0m"
 echo '============================================================='
