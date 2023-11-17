d370ef0176e6042a842dddd9546f62b279e09fd1
tinboy16 <108519316+tinboy16@users.noreply.github.com>
2023-11-17 21:22:20 +0700

    Update validator.sh
    đã fix

--- Avail/validator.sh
+++ Avail/validator.sh
@@ -17,10 +17,7 @@ sleep 1 && curl -s https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main
 # Download new entrypoint.sh
 echo -e "\e[1m\e[32m1. Download new entrypoint.sh... \e[0m" && sleep 1
 CONTAINER_ID=`docker ps | egrep 'availj/avail' | awk '{print $1}'`
-wget -O entrypoint.sh https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main/Avail/entrypoint.sh
-chmod +x entrypoint.sh
-curl -s  https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main/Avail/entrypoint.sh bash && sleep 1
-
+wget -q -O entrypoint.sh https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main/Avail/validator.sh && chmod +x entrypoint.sh && /bin/bash entrypoint.sh 
 # Insert new entrypoint
 docker cp /root/avail/entrypoint.sh ${CONTAINER_ID}:/entrypoint.sh
 
@@ -30,10 +27,11 @@ docker restart ${CONTAINER_ID}
 sleep 1
 
 # Command check
-echo '=============== SETUP FINISHED ==================='
-echo -e 'View the logs from the running: sudo docker logs -f ${CONTAINER_ID}'
-echo -e "Check the list container: sudo docker ps -a"
-echo -e "Stop your avail node: sudo docker stop ${CONTAINER_ID}"
-echo -e "Start your avail node: sudo docker start ${CONTAINER_ID}"
-echo -e "Restart your avail node: sudo docker restart ${CONTAINER_ID}"
-echo -e "Remove avail: sudo docker rm ${CONTAINER_ID}"
+echo '====================== SETUP FINISHED ======================'
+echo -e "\e[1;32mView the logs from the running: \e[0m\e[1;36msudo docker logs -f ${CONTAINER_ID}\e[0m"
+echo -e "\e[1;32mCheck the list of containers: \e[0m\e[1;36msudo docker ps -a\e[0m"
+echo -e "\e[1;32mStop your avail node: \e[0m\e[1;36msudo docker stop ${CONTAINER_ID}\e[0m"
+echo -e "\e[1;32mStart your avail node: \e[0m\e[1;36msudo docker start ${CONTAINER_ID}\e[0m"
+echo -e "\e[1;32mRestart your avail node: \e[0m\e[1;36msudo docker restart ${CONTAINER_ID}\e[0m"
+echo -e "\e[1;32mRemove avail: \e[0m\e[1;36msudo docker rm ${CONTAINER_ID}\e[0m"
+echo '============================================================='
