8a5239134bc75c8a2ad2bef3477faae46647357c
MFLow VNBnode <76662222+pnpn1212@users.noreply.github.com>
2023-11-17 22:12:46 +0700

    Update validator.sh

--- Avail/validator.sh
+++ Avail/validator.sh
@@ -8,19 +8,20 @@ echo ''
 else
   sudo apt update && sudo apt install curl -y < "/dev/null"
 fi
-
+# Logo
 sleep 1 && curl -s https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main/logo.sh | bash && sleep 1
+
 # Download new entrypoint.sh
-mkdir /root/avail
-cd /root/avail
 echo -e "\e[1m\e[32m1. Download new entrypoint.sh... \e[0m" && sleep 1
 wget -q -O entrypoint.sh https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main/Avail/entrypoint.sh
 chmod +x entrypoint.sh
 CONTAINER_ID=`docker ps | egrep 'availj/avail' | awk '{print $1}'`
+
 # Insert new entrypoint
 docker cp /root/avail/entrypoint.sh ${CONTAINER_ID}:/entrypoint.sh
 rm /root/avail/validator.sh
 rm /root/avail/entrypoint.sh
+
 # Restart avail container
 echo -e "\e[1m\e[32m2. Restart avail container... \e[0m" && sleep 1
 docker restart ${CONTAINER_ID}
