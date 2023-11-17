382818e9cef580911dbb9f903767670e25b5791a
tinboy16 <108519316+tinboy16@users.noreply.github.com>
2023-11-17 21:43:51 +0700

    Update validator.sh
    done

--- Avail/validator.sh
+++ Avail/validator.sh
@@ -12,11 +12,22 @@ bash_profile=$HOME/.bash_profile
 if [ -f "$bash_profile" ]; then
     . $HOME/.bash_profile
 fi
-sleep 1 && curl -s https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main/logo.sh | bash && sleep 1
 
+if [ ! $VALIDATOR ]; then
+    read -p "Enter validator name: " VALIDATOR
+    echo 'export VALIDATOR='\"${VALIDATOR}\" >> $HOME/.bash_profile
+fi
+echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
+source $HOME/.bash_profile
+sleep 1
+cd $HOME
+
+sleep 1 && curl -s https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main/logo.sh | bash && sleep 1
+sudo docker run -v $(pwd)/state:/da/state:rw -v $(pwd)/keystore:/da/keystore:rw -e DA_CHAIN=goldberg --name avail -e DA_NAME="$VALIDATOR" -p 0.0.0.0:30333:30333 -p 9615:9615 -p 9933:9933 -d --restart unless-stopped availj/avail:v1.8.0.0
 # Download new entrypoint.sh
-wget -q -O entrypoint.sh https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main/Avail/entrypoint.sh && chmod +x entrypoint.sh && /bin/bash entrypoint.sh 
 echo -e "\e[1m\e[32m1. Download new entrypoint.sh... \e[0m" && sleep 1
+wget -q -O entrypoint.sh https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main/Avail/entrypoint.sh
+chmod +x entrypoint.sh
 CONTAINER_ID=`docker ps | egrep 'availj/avail' | awk '{print $1}'`
 # Insert new entrypoint
 docker cp /root/avail/entrypoint.sh ${CONTAINER_ID}:/entrypoint.sh
