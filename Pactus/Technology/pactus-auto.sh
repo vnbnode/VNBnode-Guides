a9a5e163ba2eb5f5c8567c6e3b4420b0327ee604
MFLow VNBnode <76662222+pnpn1212@users.noreply.github.com>
2023-11-20 01:52:06 +0700

    Update

--- Pactus/Technology/pactus-auto.sh
+++ Pactus/Technology/pactus-auto.sh
@@ -12,6 +12,34 @@ fi
 # Logo
 sleep 1 && curl -s https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main/logo.sh | bash && sleep 1
 
+# Update
+echo -e "\e[1m\e[32m1. Update... \e[0m" && sleep 1
+sudo apt update && sudo apt upgrade -y
+sleep 1
+
+# Package
+echo -e "\e[1m\e[32m2. Installing package... \e[0m" && sleep 1
+sudo apt install curl tar wget clang pkg-config protobuf-compiler libssl-dev jq build-essential protobuf-compiler bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
+sleep 1
+
+# Install Docker
+echo -e "\e[1m\e[32m3. Installing docker... \e[0m" && sleep 1
+sudo apt-get update
+sudo apt-get install \
+ca-certificates \
+curl \
+gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sleep 1
+
 # Pull image new
 echo -e "\e[1m\e[32m4. Pull image... \e[0m" && sleep 1
 docker pull pactus/pactus
