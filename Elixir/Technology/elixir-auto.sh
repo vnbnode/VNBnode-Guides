86b778d6fb435aebc29752864e854716c205a6b4
MFLow VNBnode <76662222+pnpn1212@users.noreply.github.com>
2023-11-22 00:44:55 +0700

    Update elixir-auto.sh

--- Elixir/Technology/elixir-auto.sh
+++ Elixir/Technology/elixir-auto.sh
@@ -12,36 +12,6 @@ fi
 # Logo
 sleep 1 && curl -s https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main/logo.sh | bash && sleep 1
 
-# Set ENV ADDRESS
-if [ ! $ENVADDRESS ]; then
-    read -p "Enter ADDRESS: " ENVADDRESS
-    echo 'export ENVADDRESS='\"${ENVADDRESS}\" >> $HOME/.bash_profile
-fi
-echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
-source $HOME/.bash_profile
-sleep 1
-
-# Set ENV PRIVATE_KEY
-if [ ! $ENVPRIVATE_KEY ]; then
-    read -p "Enter PRIVATE_KEY: " ENVPRIVATE_KEY
-    echo 'export ENVPRIVATE_KEY='\"${ENVPRIVATE_KEY}\" >> $HOME/.bash_profile
-fi
-echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
-source $HOME/.bash_profile
-sleep 1
-
-# Set ENV VALIDATOR_NAME
-if [ ! $ENVVALIDATOR_NAME ]; then
-    read -p "Enter VALIDATOR_NAME: " ENVVALIDATOR_NAME
-    echo 'export ENVVALIDATOR_NAME='\"${ENVVALIDATOR_NAME}\" >> $HOME/.bash_profile
-fi
-echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
-source $HOME/.bash_profile
-sleep 1
-
-rm $HOME/set-env.sh
-echo -e "\e[1;32mDONE\e[0m"
-
 # Update
 echo -e "\e[1m\e[32m1. Update... \e[0m" && sleep 1
 sudo apt update && sudo apt upgrade -y
@@ -72,10 +42,6 @@ sleep 1
 
 # Build Dockerfile
 echo -e "\e[1m\e[32m4. Build Dockerfile... \e[0m" && sleep 1
-mkdir ev && cd ev
-sleep 1
-wget https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main/Elixir/Technology/Dockerfile
-sleep 1
 docker build . -f Dockerfile -t elixir-validator
 sleep 1
 
