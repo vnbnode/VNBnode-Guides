ac8d59842825b8259b83e6dec2ea7ba41374096d
MFLow VNBnode <76662222+pnpn1212@users.noreply.github.com>
2023-11-24 21:02:14 +0700

    Update voi-auto.sh

--- VOI/Technology/voi-auto.sh
+++ VOI/Technology/voi-auto.sh
@@ -15,25 +15,25 @@ sleep 1 && curl -s https://raw.githubusercontent.com/vnbnode/VNBnode-Guides/main
 cd $HOME
 
 # Need install software and its updates
-echo -e "\e[1m\e[32m2. Install software and its updates... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m1. Install software and its updates... \e[0m" && sleep 1
 sudo apt install -y jq bc gnupg2 curl software-properties-common
 curl -o - https://releases.algorand.com/key.pub | sudo tee /etc/apt/trusted.gpg.d/algorand.asc
 sudo add-apt-repository "deb [arch=amd64] https://releases.algorand.com/deb/ stable main"
 sleep 1
 
 # Install Node Algorand
-echo -e "\e[1m\e[32m3. Install Node Algorand... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m2. Install Node Algorand... \e[0m" && sleep 1
 sudo apt update && sudo apt install -y algorand
 sudo systemctl stop algorand && sudo systemctl disable algorand
 sleep 1
 
 # Setup goal
-echo -e "\e[1m\e[32m4. Setup goal... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m3. Setup goal... \e[0m" && sleep 1
 echo -e "\nexport ALGORAND_DATA=/var/lib/algorand/" >> ~/.bashrc && source ~/.bashrc
 sleep 1
 
 # Configure Node VOI
-echo -e "\e[1m\e[32m5. Configure Node VOI... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m4. Configure Node VOI... \e[0m" && sleep 1
 sudo algocfg set -p DNSBootstrapID -v "<network>.voi.network" -d /var/lib/algorand/ &&\
 sudo algocfg set -p EnableCatchupFromArchiveServers -v true -d /var/lib/algorand/ &&\
 sudo chown algorand:algorand /var/lib/algorand/config.json &&\
@@ -47,7 +47,7 @@ sudo sed -i 's/Algorand daemon/Voi daemon/g' /etc/systemd/system/voi.service
 sleep 1
 
 # Start Node VOI
-echo -e "\e[1m\e[32m6. Start Node VOI... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m5. Start Node VOI... \e[0m" && sleep 1
 sudo systemctl start voi && sudo systemctl enable voi
 goal node catchup $(curl -s https://testnet-api.voi.nodly.io/v2/status|jq -r '.["last-catchpoint"]')
 sleep 1
